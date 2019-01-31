9b56615bd8bdd94e0bf0a6d5c086c95a68edbadd
Patryk Buzowicz <patrykbuzowicz@gmail.com>
2019-01-31 22:03:51 +0100

    added test project

---
+++ Hyse.Tools.ExpressionReplacer.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Hyse.Tools.ExpressionReplacer.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
